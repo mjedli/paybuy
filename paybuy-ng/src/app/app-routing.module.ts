c3f516cbf49705226d1a7521b1bc7a8189ea0dcb
mjedli <jedli.mejdi.recup@gmail.com>
2022-03-08 15:53:58 +0100

    update corp application angular

--- paybuy-ng/src/app/app-routing.module.ts
+++ paybuy-ng/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MenuComponent } from './menu/menu.component';
+import { CustomerComponent } from './customer/customer.component';
+
+const routes: Routes = [
+	
+	{ path:'', redirectTo:'menu', pathMatch: 'full' },
+	{ path: 'menu', component: MenuComponent },
+ 	{ path: 'customer', component: CustomerComponent }
+ 	
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- paybuy-ng/src/app/app.module.ts
+++ paybuy-ng/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ToolbarComponent, FooterComponent, MenuComponent, CustomerComponent
   ],
   imports: [
     BrowserModule,
