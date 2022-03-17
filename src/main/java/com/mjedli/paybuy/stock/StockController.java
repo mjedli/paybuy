0e14eba36e3d69faec1d5c5cc465dcb0448b7afb
mjedli <jedli.mejdi.recup@gmail.com>
2022-03-17 16:29:55 +0100

    update application provider

---
+++ src/main/java/com/mjedli/paybuy/stock/StockController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.mjedli.paybuy.stock;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mjedli
+ *
+ */
+@RestController
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080"})
+public class StockController {
+
+}
